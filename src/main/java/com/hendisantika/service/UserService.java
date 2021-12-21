10e9c65ff200d9fa29c8d6e817e91df1318a44e1
Hendi Santika <hendisantika@gmail.com>
2021-12-21 07:50:17 +0700

    #10 - Add UserService

---
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -0,0 +1,28 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-user-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/12/21
+ * Time: 07.48
+ */
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+}
