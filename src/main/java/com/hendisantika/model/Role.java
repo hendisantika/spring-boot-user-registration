71250a2804aeb8e0634bd738e1e17aecce549549
Hendi Santika <hendisantika@gmail.com>
2021-12-21 07:41:35 +0700

    #4 - Add Role Model

---
+++ src/main/java/com/hendisantika/model/Role.java
@@ -0,0 +1,28 @@
+package com.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-user-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/12/21
+ * Time: 07.41
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
